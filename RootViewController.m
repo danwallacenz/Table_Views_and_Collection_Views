83ae354d6d57a111813137e1e68a166706e68636
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-08-10 17:50:03 +1200

    add text styling ,imageView & accessoryView

--- RootViewController.m
+++ RootViewController.m
@@ -14,31 +14,7 @@
 
 @implementation RootViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 #pragma mark - Table view data source
 
@@ -62,61 +38,59 @@
     
     if(cell == nil){
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-        cell.textLabel.textColor = [UIColor redColor];
+        
+        // text styling (label).
+        cell.textLabel.textColor = [UIColor orangeColor];
+        cell.textLabel.highlightedTextColor = [UIColor redColor];
+        cell.textLabel.textAlignment = NSTextAlignmentLeft;
+        cell.textLabel.numberOfLines = 2; // 0 = no max
+        cell.textLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:18.0];
+        cell.textLabel.shadowColor = [UIColor lightGrayColor];
+        cell.textLabel.shadowOffset = CGSizeMake(1, 1);
+        
+        // image.
+        cell.imageView.image = [self image];
+        cell.imageView.contentMode = UIViewContentModeCenter;
+        
+        // accessory type
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        
+        // accessory view. Overrides accessoryType.
+        cell.accessoryView = [self accessoryViewButton];
+        
     }
     // Configure the cell...
-    cell.textLabel.text = [NSString stringWithFormat:@"Hello there! %ld", (long)indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"Hello there!\n %ld", (long)indexPath.row];
     return cell;
 }
 
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+-(void) tap:(UIButton *)button
 {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+    NSLog(@"Accessory view button tapped %@", button);
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+-(UIButton *)accessoryViewButton
 {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"Tap Me" forState:UIControlStateNormal];
+    [button sizeToFit];
+//    [button targetForAction:@selector(tap:) withSender:button];
+    [button addTarget:self action:@selector(tap:) forControlEvents:UIControlEventTouchUpInside];
+    
+    return button;
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+-(UIImage *) image
 {
+    CGFloat side = 30;
+    UIImage *originalImage = [UIImage imageNamed:@"smiley"];
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(side,side), YES, 0);
+    [originalImage drawInRect:CGRectMake(0, 0, side, side)];
+    UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return resizedImage;
 }
-*/
 
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
