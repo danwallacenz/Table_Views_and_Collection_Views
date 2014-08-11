b209eabe4e0a22c0c358ad28a0eec136a19fd673
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-08-11 19:31:56 +1200

    cell in nib there

---
+++ Table_Views_and_Collection_Views/XibCell.m
@@ -0,0 +1,34 @@
+//
+//  XibCell.m
+//  Table_Views_and_Collection_Views
+//
+//  Created by Daniel Wallace [DATACOM] on 11/08/14.
+//  Copyright (c) 2014 nz.co.danielw. All rights reserved.
+//
+
+#import "XibCell.h"
+
+@implementation XibCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Table_Views_and_Collection_Views/XibCellTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  XibCellTableViewController.h
+//  Table_Views_and_Collection_Views
+//
+//  Created by Daniel Wallace [DATACOM] on 11/08/14.
+//  Copyright (c) 2014 nz.co.danielw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XibCellTableViewController : UITableViewController
+
+@end
